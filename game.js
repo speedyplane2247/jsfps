2548ad6425466b7962c2f25644292b2033c7d0ee
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2018-03-12 16:45:33 -0400

    1.0.2-patch2

--- game.js
+++ game.js
@@ -3,19 +3,19 @@ JSFPS
 (c) speedyplane2247 2017-2018
 Simple game
 */
-var gunSpeed = [0.5, 10, 0.9 ]; // Sniper, Machine, Pistol RPSecond
+var gunSpeed = [0.5, 10, 0.9, 0.01, 1, 0.7]; // Sniper, Machine, Pistol RPSecond
 var pos = [ 0,0, 100 ];
 var ai1 = [ 10,10, 100 ]
 var ai2 = [ 20,20, 100 ]
 var ai3 = [ 21, 55, 100]
 var ai4 = [ -2, 5, 100 ]
-var ai5 = [ -10, -10, 100 ]
+var ai5 = [ -10, -10, 100]
 var ai6 = [ -20, -20, 100]
-var weaponDamage = [1, 5, 70]
+var weaponDamage = [1, 5, 70,120,25,9]
 var HousingMapY=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,9,9,9,9,11,12,13,14,15,15,15,15,15,3,3,3,3,3,4,5,5,5,5,5,24,23,22,21,20,19,18,17,16,24,24,24,23,22,21,20,19,18,17,16,23,22,21,20,19,18,17,16,15,14,22,22,21,20,19,18,17,16,15,14,13,12,11,11,11,9,9,9,8,7,6,5,8,7,6,5,22,22,22,23,24,25];
 var HousingMapX=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,2,3,4,5,6,6,6,6,6,4,3,2,1,6,7,8,9,10,10,10,9,8,7,6,8,8,8,8,8,8,8,8,8,9,10,11,11,11,11,11,11,11,11,11,13,13,13,13,13,13,13,13,13,13,14,15,15,15,15,15,15,15,15,15,15,15,15,14,13,13,14,15,15,15,15,15,13,13,13,13,24,23,22,22,22,22];
 // The X and Y of objects in example map "Housing"
-var GameVersion = "1.0.2-patch1";
+var GameVersion = "1.0.3";
 function askForName() {
 var person = prompt("Please enter your player name", "Guest"+Math.floor(Math.random() * Math.floor(99999)));
 alert("Hello, "+ person +" !");
@@ -35,9 +35,11 @@ playGame()
 function playGame () {
 console.log("You can now play the game.")
 console.log("Use the following functions to play: moveLeft(blocks), moveRight(blocks), moveUp(blocks), moveDown(block).")
-console.log("To shoot, type shoot(gun,time). Guns are: Sniper, Machine, and Pistol. Time is in S.")
+console.log("To shoot, type shoot(gun,time). Guns are listed below. Time is in S.")
+console.log("Guns: Sniper, Pistol, Machine, Rocket, Shotgun, and Rifle.")
 console.log("To see your position, type myPos().")
 console.log("To see another player’s position, type checkRadar()")
+console.log("If you want to review this message, type help().")
 }
 function moveLeft(blocks) {
 pos[0] = pos[0] - blocks
@@ -83,6 +85,21 @@ var timeShoot = time * gunSpeed[0]
 console.log(timeShoot + " rounds were shot.")
 tellServerMoved("shot",gun,timeShoot,GameVersion)
 }
+if (gun == "Rocket") {
+    var timeShoot = time * gunSpeed[3]
+    console.log(timeShoot + " rounds were shot.")
+    tellServerMoved("shot",gun,timeShoot,GameVersion)
+    }
+    if (gun == "Shotgun") {
+        var timeShoot = time * gunSpeed[4]
+        console.log(timeShoot + " rounds were shot.")
+        tellServerMoved("shot",gun,timeShoot,GameVersion)
+        }
+        if (gun == "Rifle") {
+            var timeShoot = time * gunSpeed[5]
+            console.log(timeShoot + " rounds were shot.")
+            tellServerMoved("shot",gun,timeShoot,GameVersion)
+            }
 if (gun == "Machine") {
 var timeShoot = time * gunSpeed[1]
 console.log(timeShoot + " rounds were shot.")
@@ -117,75 +134,147 @@ if (gunX2 == "Machine") {
     ai1[2] = ai1[2] - weaponDamage[0] * timeShoot
     tellServerMoved("damage","Machine","ai1",ai1[2])
 }
+if (gunX2 == "Rocket") {
+    ai1[2] = ai1[2] - weaponDamage[3] * timeShoot
+    tellServerMoved("damage","Machine","ai1",ai1[2])
+}
+if (gunX2 == "Shotgun") {
+    ai1[2] = ai1[2] - weaponDamage[4] * timeShoot
+    tellServerMoved("damage","Machine","ai1",ai1[2])
+}
+if (gunX2 == "Rifle") {
+    ai1[2] = ai1[2] - weaponDamage[5] * timeShoot
+    tellServerMoved("damage","Machine","ai1",ai1[2])
+}
 }
 if (pos[0] == ai2[0] && pos[1] == ai2[1]) {
     if (gunX2 == "Sniper") {
         ai2[2] = ai2[2] - weaponDamage[2] * timeShoot
-        tellServerMoved("damage","Sniper","ai2",ai2[2])
+        tellServerMoved("damage",gunX2,"ai2",ai2[2])
     }
     if (gunX2 == "Pistol") {
         ai2[2] = ai2[2] - weaponDamage[1] * timeShoot
-        tellServerMoved("damage","Pistol","ai2",ai2[2])
+        tellServerMoved("damage",gunX2,"ai2",ai2[2])
     }
     if (gunX2 == "Machine") {
         ai2[2] = ai2[2] - weaponDamage[0] * timeShoot
-        tellServerMoved("damage","Machine","ai2",ai2[2])
+        tellServerMoved("damage",gunX2,"ai2",ai2[2])
+    }
+    if (gunX2 == "Rocket") {
+        ai2[2] = ai2[2] - weaponDamage[3] * timeShoot
+        tellServerMoved("damage",gunX2,"ai2",ai2[2])
+    }
+    if (gunX2 == "Shotgun") {
+        ai2[2] = ai2[2] - weaponDamage[4] * timeShoot
+        tellServerMoved("damage",gunX2,"ai2",ai2[2])
+    }
+    if (gunX2 == "Rifle") {
+        ai2[2] = ai2[2] - weaponDamage[5] * timeShoot
+        tellServerMoved("damage",gunX2,"ai2",ai2[2])
     }
 }
 if (pos[0] == ai3[0] && pos[1] == ai3[1]) {
     if (gunX2 == "Sniper") {
         ai3[2] = ai3[2] - weaponDamage[2] * timeShoot
-        tellServerMoved("damage","Sniper","ai3",ai3[2])
+        tellServerMoved("damage",gunX2,"ai3",ai3[2])
     }
     if (gunX2 == "Pistol") {
         ai3[2] = ai3[2] - weaponDamage[1] * timeShoot
-        tellServerMoved("damage","Pistol","ai3",ai3[2])
+        tellServerMoved("damage",gunX2,"ai3",ai3[2])
     }
     if (gunX2 == "Machine") {
         ai3[2] = ai3[2] - weaponDamage[0] * timeShoot
-        tellServerMoved("damage","Machine","ai3",ai3[2])
+        tellServerMoved("damage",gunX2,"ai3",ai3[2])
+    }
+    if (gunX2 == "Rocket") {
+        ai3[2] = ai3[2] - weaponDamage[3] * timeShoot
+        tellServerMoved("damage",gunX2,"ai3",ai3[2])
+    }
+    if (gunX2 == "Shotgun") {
+        ai3[2] = ai3[2] - weaponDamage[4] * timeShoot
+        tellServerMoved("damage",gunX2,"ai3",ai3[2])
+    }
+    if (gunX2 == "Rifle") {
+        ai3[2] = ai3[2] - weaponDamage[5] * timeShoot
+        tellServerMoved("damage",gunX2,"ai3",ai3[2])
     }
 }
 if (pos[0] == ai4[0] && pos[1] == ai4[1]) {
     if (gunX2 == "Sniper") {
         ai4[2] = ai4[2] - weaponDamage[2] * timeShoot
-        tellServerMoved("damage","Sniper","ai4",ai4[2])
+        tellServerMoved("damage",gunX2,"ai4",ai4[2])
     }
     if (gunX2 == "Pistol") {
         ai4[2] = ai4[2] - weaponDamage[1] * timeShoot
-        tellServerMoved("damage","Pistol","ai4",ai4[2])
+        tellServerMoved("damage",gunX2,"ai4",ai4[2])
     }
     if (gunX2 == "Machine") {
         ai4[2] = ai4[2] - weaponDamage[0] * timeShoot
-        tellServerMoved("damage","Machine","ai4",ai4[2])
+        tellServerMoved("damage",gunX2,"ai4",ai4[2])
+    }
+    if (gunX2 == "Rocket") {
+        ai4[2] = ai4[2] - weaponDamage[3] * timeShoot
+        tellServerMoved("damage",gunX2,"ai4",ai4[2])
+    }
+    if (gunX2 == "Shotgun") {
+        ai4[2] = ai4[2] - weaponDamage[4] * timeShoot
+        tellServerMoved("damage",gunX2,"ai4",ai4[2])
+    }
+    if (gunX2 == "Rifle") {
+        ai4[2] = ai4[2] - weaponDamage[5] * timeShoot
+        tellServerMoved("damage",gunX2,"ai4",ai4[2])
     }
 }
 if (pos[0] == ai5[0] && pos[1] == ai5[1]) {
     if (gunX2 == "Sniper") {
         ai5[2] = ai5[2] - weaponDamage[2] * timeShoot
-        tellServerMoved("damage","Sniper","ai5",ai5[2])
+        tellServerMoved("damage",gunX2,"ai5",ai5[2])
     }
     if (gunX2 == "Pistol") {
         ai5[2] = ai5[2] - weaponDamage[1] * timeShoot
-        tellServerMoved("damage","Pistol","ai5",ai5[2])
+        tellServerMoved("damage",gunX2,"ai5",ai5[2])
     }
     if (gunX2 == "Machine") {
         ai5[2] = ai5[2] - weaponDamage[0] * timeShoot
-        tellServerMoved("damage","Machine","ai5",ai5[2])
+        tellServerMoved("damage",gunX2,"ai5",ai5[2])
+    }
+    if (gunX2 == "Rocket") {
+        ai5[2] = ai5[2] - weaponDamage[3] * timeShoot
+        tellServerMoved("damage",gunX2,"ai5",ai5[2])
+    }
+    if (gunX2 == "Shotgun") {
+        ai5[2] = ai5[2] - weaponDamage[4] * timeShoot
+        tellServerMoved("damage",gunX2,"ai5",ai5[2])
+    }
+    if (gunX2 == "Rifle") {
+        ai5[2] = ai5[2] - weaponDamage[5] * timeShoot
+        tellServerMoved("damage",gunX2,"ai5",ai5[2])
     }
 }
 if (pos[0] == ai6[0] && pos[1] == ai6[1]) {
     if (gunX2 == "Sniper") {
         ai6[2] = ai6[2] - weaponDamage[2] * timeShoot
-        tellServerMoved("damage","Sniper","ai6",ai6[2])
+        tellServerMoved("damage",gunX2,"ai6",ai6[2])
     }
     if (gunX2 == "Pistol") {
         ai6[2] = ai6[2] - weaponDamage[1] * timeShoot
-        tellServerMoved("damage","Pistol","ai6",ai6[2])
+        tellServerMoved("damage",gunX2,"ai6",ai6[2])
     }
     if (gunX2 == "Machine") {
         ai6[2] = ai6[2] - weaponDamage[0] * timeShoot
-        tellServerMoved("damage","Machine","ai6",ai6[2])
+        tellServerMoved("damage",gunX2,"ai6",ai6[2])
+    }
+    if (gunX2 == "Rocket") {
+        ai6[2] = ai6[2] - weaponDamage[3] * timeShoot
+        tellServerMoved("damage",gunX2,"ai6",ai6[2])
+    }
+    if (gunX2 == "Shotgun") {
+        ai6[2] = ai6[2] - weaponDamage[4] * timeShoot
+        tellServerMoved("damage",gunX2,"ai6",ai6[2])
+    }
+    if (gunX2 == "Rifle") {
+        ai6[2] = ai6[2] - weaponDamage[5] * timeShoot
+        tellServerMoved("damage",gunX2,"ai6",ai6[2])
     }
 }
 if (ai1[2] < 1) {
@@ -234,4 +323,12 @@ if (playerRadar == 6) {
 console.log("AI6’s position: "+ai6[0]+" ,"+ai6[1]+".")
 }
 }
-// patch1-rename game to JSFPS
+function help() {
+console.log("You can now play the game.")
+console.log("Use the following functions to play: moveLeft(blocks), moveRight(blocks), moveUp(blocks), moveDown(block).")
+console.log("To shoot, type shoot(gun,time). Guns are: Sniper, Machine, and Pistol. Time is in S.")
+console.log("To see your position, type myPos().")
+console.log("To see another player’s position, type checkRadar()")
+console.log("If you want to review this message, type help().")
+}
+// 1.0.2-patch2 adds 3 new guns
